bb37db1335b1422aa54e920fe55f2efcd0b2a973
jaugeni <jaugeni@gmail.com>
2016-11-05 19:10:07 -0400

    add users in farebase

--- SnapchatClone2/SelectUserVC.swift
+++ SnapchatClone2/SelectUserVC.swift
@@ -7,24 +7,47 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class SelectUserVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tblView: UITableView!
+    
+    var users: [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tblView.dataSource = self
         self.tblView.delegate = self
+        
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+            
+            let user = User()
+            user.email = (snapshot.value as! NSDictionary)["email"] as! String
+            user.uid = snapshot.key
+            
+            self.users.append(user)
+            
+            self.tblView.reloadData()
+        
+        })
+        
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.email
+        
+        return cell
     }
 }
---
+++ SnapchatClone2/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  SnapchatClone2
+//
+//  Created by YAUHENI IVANIUK on 11/5/16.
+//  Copyright © 2016 be connected club. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = ""
+    var uid = ""
+    
+}
